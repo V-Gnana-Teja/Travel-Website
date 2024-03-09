a1d96a6bfb5e2b967713b5734275132b40bfb87f
V-Gnana-Teja <151600407+V-Gnana-Teja@users.noreply.github.com>
2024-03-09 17:19:18 +0530

    Created Navbar section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,69 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {BsPerson} from 'react-icons/bs'
 import {BiSearch} from 'react-icons/bi'
 import {AiOutlineClose} from 'react-icons/ai'
 import {HiOutlineMenuAlt4} from 'react-icons/hi'
-
+import {FaFacebook, FaTwitter, FaYoutube, FaPinterest, FaInstagram} from 'react-icons/fa'
 
 const Navbar = () => {
-  return (
-    <div className='flex justify-between items-center h-20 px-4'>
-       <div>
-         <h1>BEACHES.</h1>
-       </div>
+    const [nav, setNav] = useState(false);
+    const  [logo, setLogo] = useState(false);
+    
+    const handleNav = () => {
+        setNav(!nav);
+        setLogo(!logo)
+    }
 
-       <ul className=' hidden md:flex'>
-            <li>Home</li>
-            <li>Destinations</li>
-            <li>Travel</li>
-            <li>View</li>
-            <li>Book</li>
-       </ul>
+    return (
+        <div className='flex justify-between items-center h-20 px-4'>
+            <div >
+                <h1 onClick={handleNav} className={logo ? 'hidden' : 'block'}>BEACHES.</h1>
+            </div>
 
-       <div className='hidden md:flex'> 
-        <BiSearch size={20}/>
-        <BsPerson size={20}/>
-       </div>
+            <ul className='hidden md:flex'>
+                <li>Home</li>
+                <li>Destinations</li>
+                <li>Travel</li>
+                <li>View</li>
+                <li>Book</li>
+            </ul>
 
-       <div className='md:hidden '>
-            <HiOutlineMenuAlt4 size={20}/>
-       </div>
+            <div className='hidden md:flex'> 
+                <BiSearch className='mr-2' size={20}/>
+                <BsPerson size={20}/>
+            </div>
 
-       <div>
-          <ul>
-             <h1>BEACHES.</h1>
-             <li>Home</li>
-             <li>Destinations</li>
-             <li>Travel</li>
-             <li>View</li>
-             <li>Book</li>
+            {/* Hamburger */}
+            <div onClick={handleNav} className='md:hidden z-10'>
+                {nav ? <AiOutlineClose className='text-black' size={20}/> : <HiOutlineMenuAlt4 size={20}/>}   
+            </div>
 
+            {/* Mobile Menu Dropdown */}
+            <div className={nav ? 'absolute left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col' : 'hidden md:hidden' }>
+                <ul>
+                    <h1>BEACHES.</h1>
+                    <li className='border-b'>Home</li>
+                    <li className='border-b'>Destinations</li>
+                    <li className='border-b'>Travel</li>
+                    <li className='border-b'>View</li>
+                    <li className='border-b'>Book</li>
+                    
+                    <div className='flex flex-col'>  
+                        <button className='my-6'>Search</button>
+                        <button>Account</button>
+                    </div>
 
-          </ul>
-       </div>
-    </div>
-  )
+                    <div className='flex justify-between my-6'>
+                        <FaFacebook  className='icon'/>
+                        <FaTwitter className='icon'/>
+                        <FaYoutube className='icon'/>
+                        <FaPinterest className='icon'/>
+                        <FaInstagram className='icon'/>
+                    </div>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
